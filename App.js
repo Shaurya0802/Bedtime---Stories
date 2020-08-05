27fdb08a3b50e7530a87883b6e33b314b47f5e49
Shaurya0802 <shauryaakhileshsharma@gmail.com>
2020-08-05 06:39:20 +0530

    Bedtime Stories

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import WriteStoryScreen from './screens/WriteScreen';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-export default class App extends React.Component() {
+export default class App extends React.Component{
   render(){
     return(
       <AppContainer/>
